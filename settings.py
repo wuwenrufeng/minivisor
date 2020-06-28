f2511bb0539dcdd0f142703c1c2b6af10e8de5fc
wuwenrufeng <wuwenrufeng@163.com>
2020-06-28 17:23:44 +0800

    modify settings.py

--- settings.py
+++ settings.py
@@ -3,29 +3,18 @@
 
 """
 @File: settings.py
-@Project: spider_scheduler
-@Desc: 配置文件
-@Time: 2019/09/29 10:22:16
+@Project: minivisor
+@Desc: 数据库配置
+@Time: 2020/06/28 16:51:06
 @Author: guojie.bian (guojie.bian@drcnet.com.cn)
-@Last Modified: 2019/09/29 10:22:48
+@Last Modified: 2020/06/28 17:22:56
 @Modified By: guojie.bian (guojie.bian@drcnet.com.cn>)
 @Version: 1.0
-@License: Copyright(C) 2019 - 2019 DRCNET, SRIT DRCNET
+@License: Copyright(C) 2019 - 2020 DRCNET, SRIT DRCNET
 """
 
+
 #redis 配置
-REDIS_HOST = '172.16.0.4'
+REDIS_HOST = '192.168.0.4'
 REDIS_PORT = 6379
-REDIS_PWD = 'drch@rdbc1'
-# REDIS_HOST = '172.16.0.8'
-# REDIS_PORT = 6380
-
-#mongodb 配置
-MONGO_IP = "172.16.0.7"
-MONGO_PORT = 27017
-MONGO_USER = 'drch_chengdu'
-MONGO_PWD = 'drchxz#1chengdu'
-
-
-#toemails
-toemails = ["lianpengtaolpt@163.com","gaoyue8910@sina.com"]
+REDIS_PWD = 'hello'
